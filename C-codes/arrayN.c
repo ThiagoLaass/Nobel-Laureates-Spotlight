4161d90b4bd4722fb74312327a1b3f3b69e0256e
ThiagoLaass <thiagolaass202@gmail.com>
2023-09-30 23:14:34 -0300

    modified

--- C-codes/arrayN.c
+++ C-codes/arrayN.c
@@ -1,8 +1,13 @@
+#include <stdbool.h>
+#include <stdio.h>
 int main(char const *argv[])
 {
-    float num[]={1, 3, 64, 90.45, 19.6, 44530.78, 134, 13031.86, 3133.97};
     int n;
-    printf("Type N\n");
+    printf("Type N, from 0 to 8\n");
     scanf("%d", &n);
-    printf("Corresponding number %.2f", num["[%d]",n]);
+    if((n<0)&(n>8)){
+        printf("Type a valid number.");
+    }
+    float num[]={1, 3, 64, 90.45, 19.6, 44530.78, 134, 13031.86, 3133.97};
+    return printf("Corresponding number %.2f", num["[%d]",n]);
 }
