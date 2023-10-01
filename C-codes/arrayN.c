3decf60a11012f88e8f59f0fe84a983697075b1a
ThiagoLaass <thiagolaass202@gmail.com>
2023-09-30 21:16:54 -0300

    commited array rookie

---
+++ C-codes/arrayN.c
@@ -0,0 +1,8 @@
+int main(char const *argv[])
+{
+    float num[]={1, 3, 64, 90.45, 19.6, 44530.78, 134, 13031.86, 3133.97};
+    int n;
+    printf("Informe N\n");
+    scanf("%d", &n);
+    printf("Corresponding number %.2f", num["[%d]",n]);
+}
